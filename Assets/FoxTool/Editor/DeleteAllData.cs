8563fb681882814feaae8c08e15a6e512bc41d46
fox <littleyaofox@sina.com>
2017-03-18 18:32:50 +0800

    DeleteAllData

---
+++ Assets/FoxTool/Editor/DeleteAllData.cs
@@ -0,0 +1,20 @@
+﻿//==============================================================
+//  Create by fox at 2017-3-18
+//==============================================================
+
+using UnityEditor;
+using UnityEngine;
+
+namespace FoxTool
+{
+	public class DeleteAllData : MonoBehaviour
+	{
+		public const string MenuItemName = "FoxTool/DeleteAllData";
+
+		[MenuItem(MenuItemName)]
+		public static void ClearAllData()
+		{
+			PlayerPrefs.DeleteAll();
+		}
+	}
+}
