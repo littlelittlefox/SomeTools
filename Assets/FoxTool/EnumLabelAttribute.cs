bbabc40c09709e5b5ed9b8019bb2584651ccbaf0
littlelittlefox <littleyaofox@sina.com>
2017-04-13 11:02:28 +0800

    Add files via upload
    在Inspector上显示中文的属性

---
+++ Assets/FoxTool/EnumLabelAttribute.cs
@@ -0,0 +1,18 @@
+﻿//==============================================================
+//  Create by fox at 2017-4-12
+//==============================================================
+
+using UnityEngine;
+
+namespace FoxTool
+{
+    public class EnumLabelAttribute : PropertyAttribute
+    {
+        public string Name;
+
+        public EnumLabelAttribute(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ Assets/FoxTool/RenameAttribute.cs
@@ -0,0 +1,18 @@
+﻿//==============================================================
+//  Create by fox at 2017-4-12
+//==============================================================
+
+using UnityEngine;
+
+namespace FoxTool
+{
+    public class RenameAttribute : PropertyAttribute
+    {
+        public string Name;
+
+        public RenameAttribute(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
