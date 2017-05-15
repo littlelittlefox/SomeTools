6b5af741e85b0166923b5b4ae38fc87a5252b273
littlelittlefox <littleyaofox@sina.com>
2017-05-15 13:06:02 +0800

    BoolConditionAttributeDrawer

---
+++ Assets/FoxTool/Editor/BoolConditionAttributeDrawer.cs
@@ -0,0 +1,47 @@
+﻿//==============================================================
+//  Create by fox at 2017/5/14 11:07:53
+//==============================================================
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(BoolConditionAttribute))]
+// ReSharper disable once CheckNamespace
+public class BoolConditionAttributeDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        BoolConditionAttribute attr = (BoolConditionAttribute) base.attribute;
+        bool enable = CheckCondition(attr, property);
+        if ((attr.Show && enable) || (!attr.Show && !enable))
+        {
+            EditorGUI.PropertyField(position, property, label);
+        }
+    }
+
+    public bool CheckCondition(BoolConditionAttribute attr, SerializedProperty property)
+    {
+        bool enable = false;
+        SerializedProperty value = property.serializedObject.FindProperty(attr.BoolCondition);
+        enable = value != null && value.boolValue;
+        return enable;
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        BoolConditionAttribute attr = (BoolConditionAttribute)base.attribute;
+        bool enable = CheckCondition(attr, property);
+        if ((attr.Show && enable) || (!attr.Show && !enable))
+        {
+            return EditorGUI.GetPropertyHeight(property);
+        }
+        else
+        {
+            return -EditorGUIUtility.standardVerticalSpacing;
+        }
+    }
+}
