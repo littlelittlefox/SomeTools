3a8772c7ac9fd77139767f5de7ef52b65a775a3d
littlelittlefox <littleyaofox@sina.com>
2017-05-15 13:05:25 +0800

    BoolConditionAttribute

---
+++ Assets/FoxTool/BoolConditionAttribute.cs
@@ -0,0 +1,18 @@
+﻿//==============================================================
+//  Create by fox at 2017/5/14 11:01:55
+//==============================================================
+
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+public class BoolConditionAttribute : PropertyAttribute
+{
+    public string BoolCondition;
+    public bool Show;
+
+    public BoolConditionAttribute(string con, bool show)
+    {
+        this.BoolCondition = con;
+        this.Show = show;
+    }
+}
